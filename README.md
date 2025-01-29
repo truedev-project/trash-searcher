0c95545330ce83441d483d02ff051b7b222f8ded
Ksenia <yankovich.k.s@gmail.com>
2025-01-29 22:52:32 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Trash Searcher
+
+This is a project to help detect unauthorized dumps, mostly in forested areas.
---
+++ src/product/main.py
@@ -0,0 +1,6 @@
+
+def main():
+    print("Welcome to Trash Searcher, amigo! 😉")
+
+if __name__ == '__main__':
+    main()
